e04ab8528e7eebbfd7c0ea0dfa64ce794697a181
Jakub Wilczewski <jwilczewski@consdata.pl>
2019-05-12 10:59:41 +0200

    change produces

--- src/main/java/pl/threemj/webfluxexample/UserController.java
+++ src/main/java/pl/threemj/webfluxexample/UserController.java
@@ -1,6 +1,7 @@
 package pl.threemj.webfluxexample;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,14 +11,15 @@ import java.time.Duration;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
-    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<User> findAll() {
         return Flux.fromIterable(List.of(
                 User.builder().id("0001").name("Zdenek").familyname("Smetana").build(),
                 User.builder().id("0002").name("Jan").familyname("Kowalski").build()))
-                .delayElements(Duration.ofMillis(1000));
+                .delayElements(Duration.ofMillis(5000));
     }
 
 
