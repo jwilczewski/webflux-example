784677532cb817da66d50497688f528d0959d171
Jakub Wilczewski <jwilczewski@consdata.pl>
2019-05-14 16:41:44 +0200

    repair cross origin error

--- src/main/java/pl/threemj/webfluxexample/UserController.java
+++ src/main/java/pl/threemj/webfluxexample/UserController.java
@@ -3,7 +3,6 @@ package pl.threemj.webfluxexample;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
@@ -11,14 +10,15 @@ import java.time.Duration;
 import java.util.List;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:4200")
 public class UserController {
 
-    @GetMapping(produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
+    @GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    @CrossOrigin
     public Flux<User> findAll() {
         return Flux.fromIterable(List.of(
-                User.builder().id("0001").name("Zdenek").familyname("Smetana").build(),
-                User.builder().id("0002").name("Jan").familyname("Kowalski").build()))
+                User.builder().id("0001").name("Adam").familyname("Nowak").build(),
+                User.builder().id("0002").name("Jan").familyname("Kowalski").build(),
+                User.builder().id("0003").name("Andrzej").familyname("Nowakowski").build()))
                 .delayElements(Duration.ofMillis(5000));
     }
 
